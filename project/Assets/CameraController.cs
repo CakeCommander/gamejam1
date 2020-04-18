00770c1b22bce139b4a1fc3881f0ba3a501c35a5
Kenny <kenneth.mackinnon@outplay.com>
2020-04-18 13:32:30 +0100

    Camera & Player movement

---
+++ project/Assets/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _target;
+
+    [SerializeField]
+    private Vector3 offset;
+    
+    
+    void Update()
+    {
+        transform.position = _target.position + offset;
+  
+        transform.LookAt(_target);
+    }
+}
---
+++ project/Assets/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+    
+   
+
+
+    void Update()
+    {
+        if (!Input.GetMouseButton(0))
+        {
+            return;
+        }
+        
+        var mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hitInfo;
+        
+        if (!Physics.Raycast(mouseRay, out hitInfo))
+        {
+            return;
+        }
+
+        var mousePos = hitInfo.point;
+        mousePos.y = 0;
+        
+        Vector3 currentPos = transform.position;
+        currentPos.y = 0;
+
+        var dir = (mousePos - currentPos).normalized;
+
+        transform.Translate(dir*_speed*Time.deltaTime);
+    }
+}
